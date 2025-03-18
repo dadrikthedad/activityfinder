4d1bc8b1f0f41e51885c7bcdae63b3eb96079eed
Fredrik Magee <fredrikmagee@gmail.com>
2025-03-18 11:41:48 +0100

    Fikkset next.config.js

--- activitynext/next.config.js
+++ activitynext/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    output: "export", // Bruker statisk eksport
+const nextConfig = {
     images: {
-      unoptimized: true // Fjerner Next.js' innebygde bildeoptimalisering
-    }
+      domains: ["activityvercel.vercel.app"], // Tillater eksterne bilder fra din Vercel-app
+    },
+    reactStrictMode: true, // Anbefalt for å fange opp feil i utvikling
   };
+  
+  module.exports = nextConfig;
+  
   
